23c6e1e681f1fbda2c5bf7e82c4b836b09d2b2f7
Palak Basak <palakbasak8@gmail.com>
2025-03-09 13:19:40 +0600

    remove robots.txt and modified sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -4,12 +4,12 @@ export default async function sitemap() {
 
   try {
 
-    const response = await axios.get("http://localhost:3000/api/blog");
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_SITE_URL}/api/blog`);
     const { blogs } = response.data
 
     // Generate dynamic URLs for each blog
     const blogSitemap = blogs.map((blog) => ({
-      url: `http://localhost:3000/blog/${blog.slug}`,
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/${blog.slug}`,
       lastModified: new Date(blog.date).toISOString(),
       changeFrequency: "weekly",
       priority: 0.6,
@@ -17,7 +17,7 @@ export default async function sitemap() {
 
     return [
       {
-        url: 'http://localhost:3000/',
+        url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
         lastModified: new Date(),
         changeFrequency: 'yearly',
         priority: 1,
